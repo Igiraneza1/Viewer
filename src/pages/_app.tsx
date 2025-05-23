d3f30110c2d8fbade7a21dda3ab2561a02c05ee8
Igiraneza1 <igiranezaadeline@gmail.com>
2025-05-23 15:42:33 +0300

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,9 +8,8 @@ import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <SignedIn>
-        
         <div className="flex justify-end items-center bg-white shadow px-6 py-4">
           <UserButton />
         </div>
